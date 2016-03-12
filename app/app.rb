75007ff1601a00cde5109474ba6a8772788d8942
shuhei kinukawa <kinushu@gmail.com>
2016-03-12 16:27:23 +0900

    public/**/index.html をレンダリングする

--- app/app.rb
+++ app/app.rb
@@ -11,6 +11,16 @@ module Kpckara
       @app_config = settings.config
     end
 
+    get(/.*/) do
+      file_path = File.join(settings.root, '../public', request.path, 'index.html')
+      # p file_path
+      if File.exist?(file_path)
+        send_file(file_path)
+      else
+        pass
+      end
+    end
+
     # トップページ
     get '/' do
       erb :top, layout: :default
